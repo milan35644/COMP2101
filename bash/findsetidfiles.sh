7a3afcfc46cc1c1a48d1f10b516360fe1c0fda3e
milan35644 <milangabani0@gmail.com>
2019-05-16 16:30:49 -0700

    Final 1

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -11,8 +11,15 @@
 
 echo "Setuid files:"
 echo "============="
+sleep 5
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 echo "Setgid files:"
+sleep 5
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 echo "============="
+echo "TASK 2"
+sleep 5
+find / 2>/dev/null -type f -exec ls -l --block-size=M {} + | sort -rh -k 5 | head -n 10 | awk '{print $5, $3, $9}'
+#du -ha / 2>/dev/null | sort -r -k 1 | head -n 10
+#sdhfbjdskfgjkdahglkjhdflgjkhadlfkjghladfkjhgkjlsd
 echo ""
